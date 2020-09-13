90d511c00c8e7818f8d78b055cc531a4d5908d03
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-09-13 19:56:15 -0400

    axiosWithAuth and Login build

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,73 @@
 import React from "react";
+import axios from "axios"
 
-const Login = () => {
+class Login extends React.Component {
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
+ state = {
+   user: {
+     username: "",
+     password: ""
+   }
+ };
+
+ handleChange =(e) => {
+   this.setState({
+     user: {
+       ...this.state.user,
+       [e.target.name]: e.target.value
+     }
+   });
+ };
+
+ login = (e) => {
+   e.preventDefault();
+   axios
+    .post("http://localhost:5000/api/login", this.state.user)
+    .then((res)=> {
+      localStorage.setItem("token", res.data.payload);
+      //this.props.history.push("/friendsList")
+        
+    })
+    .catch((err)=> {
+      console.log("oh shit! Login.js has an error:", err)
+    })
+
+   };
+
+
+ 
+
+
+
+   render(){
+      return (
+        <>
+          <h1>Welcome to the Bubble App!</h1>
+          <form onSubmit = {this.login}>
+            <input
+              type = "text"
+              name = "username"
+              placeholder = "username"
+              value = {this.state.user.username}
+              onChange = {this.handleChange}
+              />
+            <input
+              type = "text"
+              name = "password"
+              placeholder = "password"
+              value = {this.state.user.password}
+              onChange = {this.handleChange}
+              />
+            <button>Log In</button>
+          </form>
+        </>
+      );
+
+    }
+
+
 };
 
 export default Login;
