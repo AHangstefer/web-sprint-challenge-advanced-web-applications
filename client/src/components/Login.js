7fdd3c177e9f7d3e4c65c0f56757233957bfe64d
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-09-13 20:10:12 -0400

    login, axiosWithAuth, and PrivateRoute functioning

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -27,7 +27,7 @@ class Login extends React.Component {
     .post("http://localhost:5000/api/login", this.state.user)
     .then((res)=> {
       localStorage.setItem("token", res.data.payload);
-      //this.props.history.push("/friendsList")
+      this.props.history.push("/bubblepage")
         
     })
     .catch((err)=> {
